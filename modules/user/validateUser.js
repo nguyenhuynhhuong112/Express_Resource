f5ba58b422b1219a4a60122c0ee271648a4a0f85
Huỳnh Hương Nguyễn <huynhhuong205k@gmail.com>
2024-06-28 23:15:49 +0700

    add validate to router update

--- modules/user/validateUser.js
+++ modules/user/validateUser.js
@@ -29,6 +29,34 @@ function validateUser(req, res, next) {
   next();
 }
 
+function validateUserUpdate(req, res, next) {
+  const { userName, email, password } = req.body;
+  let errors = [];
+
+  if (userName && typeof userName !== "string") {
+    errors.push({ message: "Username must be a string." });
+  }
+
+  if (email && !validator.isEmail(email)) {
+    errors.push({ message: "Valid email is required." });
+  } else if (email && !email.endsWith("@gmail.com")) {
+    errors.push({ message: "Email must end with @gmail.com." });
+  }
+
+  if (password && (typeof password !== "string" || password.length < 8)) {
+    errors.push({
+      message: "Password must be a string and at least 8 characters long.",
+    });
+  }
+
+  if (errors.length > 0) {
+    return res.status(400).json({ errors });
+  }
+
+  next();
+}
+
 module.exports = {
   validateUser,
+  validateUserUpdate,
 };
